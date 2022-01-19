528cc9b20382ce946d34efe28dbc32a410ccc07a
Pierre <roques813@gmail.com>
2022-01-19 14:58:09 +0100

    minor changes

--- main.py
+++ main.py
@@ -3,23 +3,5 @@ from src.Tweet import Tweet
 from src.User import User
 from config import api, lists
 
-# Pour démarrer le premier filtrage
-# (Pour l'arrêter, il faut interrompre le programme)
-
-# Stream.start()
-
-# Pour démarrer le second filtrage
-
-# Filtering.start()
-
-# Pour récupérer les 10 personnes qui apparaissent le plus de fois dans notre base
-
-# top = db.getTopAuthors()
-# print([e.username for e in top])
-
-
-# TESTS
-
-# test = User(username="tony84fils2pute")
-# UserLookup.getSensitiveTweetPercentage(test)
-# [(e.username,UserLookup.getSensitiveTweetPercentage(e)) if not(e.isBanned()) else (e.username, 'BANNI') for e in db.getTopAuthors(10)]
+# Documentation à venir
+# Lire le README.md
